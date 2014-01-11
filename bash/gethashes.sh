d748f728596942487b27759bb1a206d6cc611974
Leonard Lee <sheeeng@gmail.com>
2014-01-11 15:01:32 +0100

    Fix typographical error.

--- bash/gethashes.sh
+++ bash/gethashes.sh
@@ -4,9 +4,9 @@ SAVEDIFS=$IFS
 IFS=$(echo -en "\n\b")
 #FILES=*
 
-FILES=$(find . ! -name "*~" ! -name "*.sh" ! -name "*.swp" ! -name 'SHA1SUM' ! -name 'MD5SUM' -type f -exec basename {} \; | sort)
+FILES=$(find . ! -name "*~" ! -name "*.sh" ! -name "*.swp" ! -name 'SHA1SUMS' ! -name 'MD5SUMS' -type f -exec basename {} \; | sort)
 
-for hashFile in SHA1SUM MD5SUM
+for hashFile in SHA1SUMS MD5SUMS
 do
     echo "Processing $hashFile file."
     #Delete certain file(s).
@@ -16,16 +16,16 @@ done
 for file in $FILES
 do
     #Ignoring certain file(s).
-    if [ "$file" == "SHA1SUM" ] || [ "$file" == "MD5SUM" ] ; then
+    if [ "$file" == "SHA1SUMS" ] || [ "$file" == "MD5SUMS" ] ; then
         echo "Ignoring $file file."
         continue;
     fi
     echo "Processing $file file."
     #Process each file. The $file store current file name.
     #md5sum $file | tee --append MD5SUM
-    md5sum $file >> MD5SUM
+    md5sum $file >> MD5SUMS
     #sha1sum $file | tee --append SHA1SUM
-    sha1sum $file >> SHA1SUM
+    sha1sum $file >> SHA1SUMS
 done
 
 IFS=$SAVEDIFS
