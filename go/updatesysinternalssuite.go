f5ffce8da66cbd42f605bf981543c716f0dd7730
Leonard Lee <sheeeng@gmail.com>
2015-07-23 10:40:20 +0200

    Add udownload Sysinternals Suite script.

---
+++ go/updatesysinternalssuite.go
@@ -0,0 +1,53 @@
+// https://github.com/thbar/golang-playground/blob/master/download-files.go
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func downloadFromUrl(url string) {
+	tokens := strings.Split(url, "/")
+	fileName := tokens[len(tokens)-1]
+	fmt.Println("Downloading", url, "to", fileName)
+
+	// TODO: check file existence first with io.IsExist
+	output, err := os.Create(fileName)
+	if err != nil {
+		fmt.Println("Error while creating", fileName, "-", err)
+		return
+	}
+	defer output.Close()
+
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error while downloading", url, "-", err)
+		return
+	}
+	defer response.Body.Close()
+
+	n, err := io.Copy(output, response.Body)
+	if err != nil {
+		fmt.Println("Error while downloading", url, "-", err)
+		return
+	}
+
+	fmt.Println(n, "bytes downloaded.")
+}
+
+// func main() {
+// 	countries := []string{"GB", "FR", "ES", "DE", "CN", "CA", "ID", "US"}
+// 	for i := 0; i < len(countries); i++ {
+// 		url := "http://download.geonames.org/export/dump/" + countries[i] + ".zip"
+// 		downloadFromUrl(url)
+// 	}
+// }
+
+func main() {
+	url := "https://download.sysinternals.com/files/SysinternalsSuite.zip"
+	downloadFromUrl(url)
+}
